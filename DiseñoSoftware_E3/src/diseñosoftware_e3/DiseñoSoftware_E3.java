e0be24134a285971a337d5527ac5f3436b3a79c4
KEVIN <KEVIN@HOMEWORK>
2022-08-08 09:50:54 -0500

    //Prueba

---
+++ DiseñoSoftware_E3/src/diseñosoftware_e3/DiseñoSoftware_E3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package diseñosoftware_e3;
+
+/**
+ *
+ * @author KEVIN
+ */
+public class DiseñoSoftware_E3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Hola bro probando
+    }
+    
+}
